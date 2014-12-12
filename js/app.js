dc99cc3a199fa7d06e6cb1f06bf86e1ec6b2855d
David Eads <davideads@gmail.com>
2014-12-12 17:53:12 -0500

    work in progress, building out chapter card

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
+var $window;
 var $videos;
+var $intro;
+
+// Resize intro box
+var fitIntro = function(e) {
+  var height = $window.height();
+  $intro.height(height);
+}
 
 $(function() {
   // Populate variables
+  $window = $(window);
   $videos = $('.video');
+  $intro = $('.intro');
 
   // Behaviors
   $videos.fitVids();
+  fitIntro();
+  $window.on('resize', fitIntro);
 });
